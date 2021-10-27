c92b3f2da5dd298b01257c2cf5b5a761f4be7403
Nilima Jha <njha2@dons.usfca.edu>
2021-10-26 22:16:35 -0700

    Create HTTPResponse class.

--- src/main/java/server/HTTPResponse.java
+++ src/main/java/server/HTTPResponse.java
@@ -1,2 +1,23 @@
-package server;public class HTTPResponse {
+package server;
+
+public class HTTPResponse {
+    private String protocol;
+    private String statusCode;
+    private String reasonPhrase;
+    private String responseMessage;
+
+    public HTTPResponse(String protocol, String statusCode, String reasonPhrase) {
+        this.protocol = protocol;
+        this.statusCode = statusCode;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage() {
+
+    }
+
 }
