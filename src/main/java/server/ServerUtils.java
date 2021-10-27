5519fdf0048a85c39764b96b678ba1833e99f03d
Nilima Jha <njha2@dons.usfca.edu>
2021-10-26 20:16:50 -0700

    Create ServerUtils class.

--- src/main/java/server/ServerUtils.java
+++ src/main/java/server/ServerUtils.java
@@ -1,2 +1,16 @@
-package server;public class ServerUtils {
+package server;
+
+import java.io.PrintWriter;
+
+public class ServerUtils {
+
+    /**
+     * Send the status line of an HTTP 200 OK response.
+     * @param writer
+     */
+    public static void send200(PrintWriter writer) {
+        writer.printf("%s %s\r\n", HTTPConstants.VERSION, HTTPConstants.OK);
+        writer.printf("%s \r\n\r\n", HTTPConstants.CONNECTION_CLOSE);
+    }
+
 }
