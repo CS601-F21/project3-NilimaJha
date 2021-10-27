92db16853c0f111885b38eda5c3b77fde5b8b970
Nilima Jha <njha2@dons.usfca.edu>
2021-10-26 20:04:06 -0700

    Create Handler Interface.

---
+++ src/main/java/server/Handler.java
@@ -0,0 +1,10 @@
+package server;
+
+/**
+ * Handler Interface
+ * will be implemented by various Application server for different API
+ * @author nilimajha
+ */
+public interface Handler {
+    public HTTPResponse handle(HTTPRequest httpRequest);
+}
