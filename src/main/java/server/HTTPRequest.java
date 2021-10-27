908cdac940a04d12a5ce308c1ee7452517280a04
Nilima Jha <njha2@dons.usfca.edu>
2021-10-26 20:07:28 -0700

    Create HTTPRequest class.

--- src/main/java/server/HTTPRequest.java
+++ src/main/java/server/HTTPRequest.java
@@ -1,2 +1,42 @@
-package server;public class HTTPRequest {
+package server;
+
+import java.util.HashMap;
+
+public class HTTPRequest {
+    private String method;
+    private String path;
+    private HashMap<String, String> requestHeaders =new HashMap<>();
+    private String message;
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public HashMap<String, String> getRequestHeaders() {
+        return requestHeaders;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void putRequestHeaders(String header, String value) {
+        this.requestHeaders.put(header, value);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
