6580379bab17a3a28697abe347cb604deb04d2d3
Nilima Jha <njha2@dons.usfca.edu>
2021-10-27 18:08:20 -0700

    Update HTTPResponse class.

--- src/main/java/server/HTTPResponse.java
+++ src/main/java/server/HTTPResponse.java
@@ -3,21 +3,40 @@ package server;
 public class HTTPResponse {
     private String protocol;
     private String statusCode;
-    private String reasonPhrase;
+    private String statusMessage;
     private String responseMessage;
 
-    public HTTPResponse(String protocol, String statusCode, String reasonPhrase) {
+    public HTTPResponse (String protocol, String statusCode, String statusMessage) {
         this.protocol = protocol;
         this.statusCode = statusCode;
-        this.reasonPhrase = reasonPhrase;
+        this.statusMessage = statusMessage;
+    }
+
+    public String getResponseHeader() {
+        return protocol + " " + statusCode + " " + statusMessage + "\n\r\n";
+    }
+
+    public void setResponseMessage(String responseMessage) {
+//        this.responseMessage = "<!DOCTYPE html>\n" +
+//                "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+//                "<head>\n" +
+//                "  <title>Resource not found</title>\n" +
+//                "</head>\n" +
+//                "<body>\n" +
+//                "\n" +
+//                "  <p>The resource you are looking for was not found.</p>\n" +
+//                "\n" +
+//                "</body>\n" +
+//                "</html>";
+        this.responseMessage = responseMessage;
     }
 
     public String getResponseMessage() {
         return responseMessage;
     }
 
-    public void setResponseMessage() {
-
+    public String getHTTPResponse () {
+        return getResponseHeader() + getResponseMessage();
     }
 
 }
