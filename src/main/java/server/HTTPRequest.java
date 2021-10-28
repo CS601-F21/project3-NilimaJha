1297dd404a42a43fdcd27dad59801372016bf2bb
Nilima Jha <njha2@dons.usfca.edu>
2021-10-27 18:07:04 -0700

    Add isValid attribute in HTTPRequest class.

--- src/main/java/server/HTTPRequest.java
+++ src/main/java/server/HTTPRequest.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 public class HTTPRequest {
     private String method;
     private String path;
+    private String protocol;
     private HashMap<String, String> requestHeaders =new HashMap<>();
     private String message;
+    private boolean isValid = true;
 
     public String getMethod() {
         return method;
@@ -14,6 +16,10 @@ public class HTTPRequest {
 
     public String getPath() {
         return path;
+    }
+
+    public String getProtocol() {
+        return protocol;
     }
 
     public HashMap<String, String> getRequestHeaders() {
@@ -24,6 +30,10 @@ public class HTTPRequest {
         return message;
     }
 
+    public boolean getIsValid() {
+        return isValid;
+    }
+
     public void setMethod(String method) {
         this.method = method;
     }
@@ -32,11 +42,19 @@ public class HTTPRequest {
         this.path = path;
     }
 
+    public void  setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
     public void putRequestHeaders(String header, String value) {
         this.requestHeaders.put(header, value);
     }
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public void setIsValid(boolean isValid) {
+        this.isValid = isValid;
     }
 }
