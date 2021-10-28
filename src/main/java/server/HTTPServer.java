0c363f67b99c7657fd4910c4dddc0377fc48b66b
Nilima Jha <njha2@dons.usfca.edu>
2021-10-28 01:00:37 -0700

    Make Class HTTPServer more modular.

--- src/main/java/server/HTTPServer.java
+++ src/main/java/server/HTTPServer.java
@@ -37,119 +37,26 @@ public class HTTPServer {
         }
     }
 
-    /**
-     *
-     * @param instream
-     * @return
-     * @throws IOException
-     */
-    public HTTPRequest readRequest(BufferedReader instream) throws IOException {
-        HTTPRequest httpRequest= new HTTPRequest();
-        String[] HTTPRequestLineArray;
-        String currentLine;
-        String[] lastLine;
 
-        currentLine = instream.readLine();
-        HTTPRequestLineArray = currentLine.split("\\s");
-        //validating request and extracting required info.
-        if(requestLineValidator(HTTPRequestLineArray)) {
-            httpRequest.setMethod(HTTPRequestLineArray[0]);
-            httpRequest.setPath(HTTPRequestLineArray[1]);
-            httpRequest.setProtocol(HTTPRequestLineArray[2]);
-        } else {
-            httpRequest.setIsValid(false);
-        }
-        lastLine = HTTPRequestLineArray;
-        currentLine = instream.readLine();
-        while (currentLine != null) {
-            String[] HTTPRequestHeader = currentLine.split("\\s");
-            if (requestHeaderLineValidator(HTTPRequestHeader)) {
-                httpRequest.putRequestHeaders(HTTPRequestHeader[0], HTTPRequestHeader[1]);
-            } else {
-                httpRequest.setIsValid(false);
-            }
-            lastLine = HTTPRequestHeader;
-            currentLine = instream.readLine();
-        }
-       // checking if lastLine was content-length line
-        if (lastLine[0].equals("Content-length:")) {
-            int contentLength = Integer.parseInt (lastLine[1]);
-            char[] bodyArray = new char[contentLength];
-            instream.read (bodyArray, 0, bodyArray.length);
-            httpRequest.setMessage(new String (bodyArray));
-        }
-        return httpRequest;
-    }
-
-    public boolean requestLineValidator (String[] HTTPRequestArray) {
-        if (HTTPRequestArray.length == 3 && HTTPRequestArray[2].equals(HTTPConstants.PROTOCOL)) {
-             return true;
-        }
-        return false;
-    }
-
-    public boolean requestHeaderLineValidator (String[] HTTPRequestHeader) {
-        if (HTTPRequestHeader.length == 2) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * method startup()
-     */
     public void startup() {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
+            int i = 1;
             while (running) {
-                try (Socket connectionSocket = serverSocket.accept()) {
-                    this.threadPool.execute(new Runnable() {
-                        Socket currentConnectionSocket = connectionSocket;
-                        @Override
-                        public void run() {
-                            try (BufferedReader inStream = new BufferedReader(new InputStreamReader(currentConnectionSocket.getInputStream()));
-                                 PrintWriter writer = new PrintWriter(currentConnectionSocket.getOutputStream())
-                            ) {
-                                HTTPRequest httpRequest = readRequest(inStream);
-                                pathHandlerAssigner(httpRequest, writer);
-                                //got request object not check if the request is valid or not then assign a proper handler
-                            } catch (IOException e) {
-                                //logger
-                                e.printStackTrace();
-                               // HTTPResponse httpResponse = new InternalServerErrorHandler().handle(httpRequest);
-                            }
-                        }
-
-                        /**
-                         * method pathHandlerAssigner()
-                         *
-                         * @param httpRequest
-                         * @param writer
-                         */
-                        public void pathHandlerAssigner(HTTPRequest httpRequest, PrintWriter writer) {
-                            HTTPResponse httpResponse;
-                            if (!httpRequest.getIsValid()) {
-                                httpResponse = new BadRequestHandler().handle(httpRequest);
-                            } else {
-                                String path = httpRequest.getPath();
-                                if(!path.equals("GET") || !path.equals("POST")) {
-                                    httpResponse = new MethodNotAllowedHandler().handle(httpRequest);
-                                } else if (!pathToHandlerMap.containsKey(path)) {
-                                    httpResponse = new BadRequestHandler().handle(httpRequest);
-                                } else {
-                                    httpResponse = pathToHandlerMap.get(path).handle(httpRequest);
-                                }
-                            }
-                            writer.write(httpResponse.getHTTPResponse());
-                        }
-                    });
+                System.out.println(i + ": Server is waiting for input on "+ port + "....");
+                try {
+                    Socket connectionSocket = serverSocket.accept();
+                    this.threadPool.execute(new Connection(connectionSocket, this.pathToHandlerMap));
+                    System.out.println("Request Assigned to thread pool.");
                 } catch (IOException e) {
                     //logger
+                    System.out.println("CATCH IOException 1 : " + e );
                     e.printStackTrace();
                 }
-                //models view controller
+                i++;
             }
         }  catch (IOException e) {
+            System.out.println("CATCH IOException 2 : " + e );
             e.printStackTrace();
             //logger
         }
