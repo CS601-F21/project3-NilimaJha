e1bb3c1f20d9f707215bf37d31a4405e49e3c22d
Nilima Jha <njha2@dons.usfca.edu>
2021-10-29 22:44:33 -0700

    Make small changes in the TestHandler class and driver class Test.

--- src/main/java/server/Test.java
+++ src/main/java/server/Test.java
@@ -1,13 +1,34 @@
 package server;
 
+import searchApplication.api.ReviewSearchHandler;
+import searchApplication.invertedIndex.*;
+import searchApplication.api.FindHandler;
+
+import java.io.IOException;
+
 public class Test {
     public static void main(String[] args) {
+
+        ReviewFileData rfds = new ReviewFileData();
+        QAFileData qafds = new QAFileData();
+        String reviewFileName = "reviews_Cell_Phones_and_Accessories_5.json";
+        String qaFileName = "qa_Cell_Phones_and_Accessories.json";
+        try {
+            rfds = new FileProcessor().reviewFileProcessor(reviewFileName);
+            qafds = new FileProcessor().qaFileProcessor(qaFileName);
+        } catch (IOException e) {
+            System.out.println("IOException." + e);
+        }
         HTTPServer httpServer = new HTTPServer(1026);
+        httpServer.addMapping("/find", new FindHandler(rfds, qafds));
+        httpServer.addMapping("/reviewsearch", new ReviewSearchHandler(rfds,qafds));
+        System.out.println();
+        System.out.println("Test server starting.");
+        System.out.println("........................");
+        httpServer.startup();
         System.out.println("Test server started.");
         System.out.println("........................");
-        httpServer.addMapping("/find", new TestHandler());
-        System.out.println();
-        httpServer.startup();
     }
+
 
 }
--- src/main/java/server/TestHandler.java
+++ src/main/java/server/TestHandler.java
@@ -9,14 +9,20 @@ public class TestHandler implements Handler {
         String responseStatusMessage = HTTPConstants.MESSAGE_OK;
         HTTPResponse httpResponse = new HTTPResponse(responseProtocol, responseStatusCode, responseStatusMessage);
         String responseMessage;
+
         responseMessage = "<!DOCTYPE html>\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                 "<head>\n" +
-                "  <title>Test</title>\n" +
+                "  <title>Find</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
                 "\n" +
-                "  <p>Test server worked.</p>\n" +
+                "<h1><u>Find ASIN</u></h1>" +
+                "<form action=\"/echo\" method=\"post\">\n" +
+                "  <label for=\"msg\"><b>Enter ASIN:</b></label><br/><br/>\n" +
+                "  <input type=\"text\" id=\"msg\" name=\"msg\"/><br/><br/>\n" +
+                "  <input type=\"submit\" value=\"Submit\"/>\n" +
+                "</form>" +
                 "\n" +
                 "</body>\n" +
                 "</html>";
