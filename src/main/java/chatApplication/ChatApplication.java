b99d3b303eb7893c8a1e645f596aa4985df24f7f
Nilima Jha <njha2@dons.usfca.edu>
2021-10-29 22:42:11 -0700

    Create chat application driver class.

--- src/main/java/chatApplication/ChatApplication.java
+++ src/main/java/chatApplication/ChatApplication.java
@@ -1,2 +1,18 @@
-package chatApplication;public class ChatApplication {
+package chatApplication;
+
+import chatApplication.api.SlackBotHandler;
+import server.HTTPServer;
+
+public class ChatApplication {
+    public static void main(String[] args) {
+        HTTPServer httpServer = new HTTPServer(1027);
+        httpServer.addMapping("/slackbot", new SlackBotHandler());
+        System.out.println();
+        System.out.println("Test server starting.");
+        System.out.println("........................");
+        httpServer.startup();
+        System.out.println("Test server started.");
+        System.out.println("........................");
+    }
+
 }
