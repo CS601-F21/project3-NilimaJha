9ca948d02534f05fa388afe2dc316dfe2dfc5baa
Nilima Jha <njha2@dons.usfca.edu>
2021-11-01 22:16:37 -0700

    Add doPostOnSlack method to do Post call on the slack api.

--- src/main/java/chatApplication/api/SlackBotHandler.java
+++ src/main/java/chatApplication/api/SlackBotHandler.java
@@ -1,9 +1,22 @@
 package chatApplication.api;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import handler.Handler;
 import server.HTTPConstants;
 import server.HTTPRequest;
 import server.HTTPResponse;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SlackBotHandler implements Handler {
 
@@ -13,7 +26,7 @@ public class SlackBotHandler implements Handler {
             return doGet();
         } else {
            // return doPost(httpRequest.getRequestPayload());
-            return doPost();
+            return doPost(httpRequest.getRequestPayload());
         }
     }
 
@@ -46,7 +59,30 @@ public class SlackBotHandler implements Handler {
         return HTMLResponseMessage;
     }
 
-    public HTTPResponse doPost() {
+    public HTTPResponse doPost(String httpRequestMessage) {
+
+        String token = "xoxb-2464212157-2698004405792-VHX1nJ2jZkHuBUSTYHbPJhbD";
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " +token);
+        System.out.println("================================");
+        System.out.println(headers.get("Authorization"));
+        System.out.println("================================");
+
+        headers.put("Content-Type", "application/json");
+        headers.put("Accept-Charset", "utf-8");
+        //headers.put("Connection", "close");
+        System.out.println("Size of header Map: "+headers.size());
+        String url = "https://slack.com/api/chat.postMessage";
+        JsonObject obj = new JsonObject();
+        obj.addProperty("channel", "C02KR8EDTNZ");
+        obj.addProperty("text", httpRequestMessage);
+        String body = obj.toString();
+        System.out.println("+++++++++++++++++++++++++++++++++++");
+        System.out.println(body);
+        String slackPostResponse = doPostOnSlack(url, headers, body);
+        //parse response;
+        System.out.println(slackPostResponse);
+
         String responseProtocol = HTTPConstants.PROTOCOL;
         String responseStatusCode = HTTPConstants.CODE_OK;
         String responseStatusMessage = HTTPConstants.MESSAGE_OK;
@@ -65,5 +101,67 @@ public class SlackBotHandler implements Handler {
                 "</html>";
         httpResponse.setResponseMessage(HTMLResponseMessage);
         return httpResponse;
+
+    }
+
+    public static String doPostOnSlack(String url, Map<String, String> headers, String body) {
+        try {
+            System.out.println("Inside do post method");
+            HttpRequest.Builder builder = HttpRequest.newBuilder(new URI(url));
+            System.out.println("Created Builder.");
+            System.out.println("Size of header Map before sending it to setHeaders method: "+headers.size());
+            builder = setHeaders(builder, headers);
+            System.out.println("Header Set.");
+            HttpRequest request = builder.POST((HttpRequest.BodyPublishers.ofString(body))).build();
+            System.out.println("Set method as Post in request.");
+            System.out.println("--------->" + request.method());
+            System.out.println("--------->" + request.headers());
+            System.out.println("--------->" + request.uri());
+            System.out.println("--------->" + request.bodyPublisher());
+
+            HttpClient client = HttpClient.newHttpClient();
+            System.out.println("Sending the request to slack.");
+            //System.out.println("HttpResponse.BodyHandlers.ofString() :" + HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("-.-.-.-.-.-.-.-.->Body      "+response.body());
+            System.out.println("-.-.-.-.-.-.-.-.->uri       "+response.uri());
+            System.out.println("-.-.-.-.-.-.-.-.->SC        "+response.statusCode());
+
+            return response.body();
+
+        } catch(URISyntaxException | IOException | InterruptedException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+
+    }
+
+
+    private static HttpRequest.Builder setHeaders (HttpRequest.Builder builder, Map < String, String > headers){
+        System.out.println("Inside setHeaders method.");
+        System.out.println("Size of header Map: "+headers.size());
+        if (headers != null) {
+            System.out.println("header map provided is not null.");
+            int i = 1;
+            for (String key : headers.keySet()) {
+                System.out.println(i +"Headers key:" +key);
+                builder = builder.setHeader(key, headers.get(key));
+                System.out.println("Headers value:" +headers.get(key));
+                i++;
+            }
+            System.out.println("Header set done.");
+        }
+        System.out.println("returning builder object");
+        return builder;
+    }
+
+    private String parseSlackResponse (String slackPostResponse) {
+//        if (slackPostResponse.contains("ok")) {
+//
+//        }
+        //String channel =
+        return slackPostResponse;
     }
 }
+
