6f6146f68227ae526dfcc0b0216a0007d3a2c880
Nilima Jha <njha2@dons.usfca.edu>
2021-11-02 20:39:31 -0700

    Add SlackResponse class to store the response received from Slack API in json format.

--- src/main/java/chatApplication/api/SlackResponse.java
+++ src/main/java/chatApplication/api/SlackResponse.java
@@ -1,5 +1,32 @@
 package chatApplication.api;
 
 public class SlackResponse {
+    private boolean ok = false;
+    private String text = null;
+    private String error = null;
+
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public boolean isOk() {
+        return ok;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getError() {
+        return error;
+    }
 
 }
