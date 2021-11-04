2aaba5447bfe0dd71ddb1bb5cc1115f92cd52e09
Nilima Jha <njha2@dons.usfca.edu>
2021-11-04 02:26:32 -0700

    Change the value of some constants.

--- src/main/java/server/HTTPConstants.java
+++ src/main/java/server/HTTPConstants.java
@@ -1,5 +1,7 @@
 package server;
 
+import java.util.HashMap;
+
 /**
  * A helper class to store various constants used for the HTTP server.
  * @author nilimajha
@@ -9,23 +11,26 @@ public class HTTPConstants {
     public static final String POST = "POST";
     public static final String PROTOCOL = "HTTP/1.1";
 
-    public static final String CODE_OK = "200";
+    public static final int CODE_OK = 200;
     public static final String MESSAGE_OK = "OK";
 
-    public static final String CODE_SERVER_ERROR = "500";
+    public static final int CODE_SERVER_ERROR = 500;
     public static final String MESSAGE_SERVER_ERROR = "Internal Server Error";
 
-    public static final String CODE_NOT_ALLOWED = "405";
+    public static final int CODE_NOT_ALLOWED = 405;
     public static final String MESSAGE_NOT_ALLOWED = "Method Not Allowed";
 
-    public static final String CODE_NOT_FOUND = "404";
+    public static final int CODE_NOT_FOUND = 404;
     public static final String MESSAGE_NOT_FOUND = "Not Found";
 
-    public static final String CODE_BAD_REQUEST = "400";
+    public static final int CODE_BAD_REQUEST = 400;
     public static final String MESSAGE_BAD_REQUEST = "Bad Request";
 
     public static final String CONTENT_LENGTH = "Content-Length:";
-    public static final String CONNECTION_CLOSE = "Connection: closed";
+    public static final String CONNECTION_CLOSE = "Connection: closed\s";
+
+    public static final int CHAT_APP_PORT = 9090;
+    public static final int SEARCH_APP_PORT = 8080;
 
 
 }
