d685cf5e8f8a8846c574b83872c1c8cedc09e7e3
Nilima Jha <njha2@dons.usfca.edu>
2021-11-04 02:33:42 -0700

    Add test cases for chatApplication.

--- src/test/java/chatApplicationTest/ChatApplicationTest.java
+++ src/test/java/chatApplicationTest/ChatApplicationTest.java
@@ -1,39 +1,22 @@
-package searchApplicationTest;
+package chatApplicationTest;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import server.HTTPResponse;
+import server.HTTPTestClient;
 
-import searchApplication.invertedIndex.FileDataOps;
-import searchApplication.invertedIndex.FileProcessor;
-
-import server.*;
-import server.httpDefaultHandlers.MethodNotAllowedHandler;
-import server.httpDefaultHandlers.PathNotFoundHandler;
-
-import java.io.IOException;
 import java.util.HashMap;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SearchApplicationFindTest {
-    public static FileDataOps fileOps;
+
+public class ChatApplicationTest {
     public static String PathNotFoundResponse404;
     public static String BadRequestResponse400;
     public static String MethodNotAllowedResponse405;
 
     @BeforeAll
-    public static void InitializeInvertedIndex() {
-        FileProcessor fileProcessor = new FileProcessor();
-        try {
-            fileProcessor.reviewFileProcessor("review_Cell_Phones_and_Accessories.json");
-            fileProcessor.qaFileProcessor("qa_Cell_Phones_and_Accessories_5.json");
-        } catch (IOException e) {
-            System.out.println("IOException occurred while reading from input files.");
-            System.out.println("IOException." + e);
-            System.exit(0);
-        }
-        fileOps = new FileDataOps();
-
+    public static void TestInitializer() {
         PathNotFoundResponse404 = "<!DOCTYPE html>\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                 "<head>\n" +
@@ -73,55 +56,52 @@ public class SearchApplicationFindTest {
 
     //for /find
     @Test
-    public void testFindAsinForGETWithCorrectPath() {
+    public void testSlackBotForGETWithCorrectPath() {
         String expectedResponse = "<!DOCTYPE html>\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                 "<head>\n" +
-                "  <title>Find ASIN</title>\n" +
+                "  <title>Slack Bot</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
                 "\n" +
-                "<h1><u>Find ASIN</u></h1>" +
-                "<form action=\"/find\" method=\"post\">\n" +
-                "  <label for=\"msg\"><b>Enter ASIN:</b></label><br/><br/>\n" +
-                "  <input type=\"text\" id=\"asin\" name=\"asin\"/><br/><br/>\n" +
+                "<h1><u>Slack Bot</u></h1><form action=\"/slackbot\" method=\"post\">\n" +
+                "  <label for=\"msg\"><b>Enter Message:</b></label><br/><br/>\n" +
+                "  <input type=\"text\" id=\"message\" name=\"message\"/><br/><br/>\n" +
                 "  <input type=\"submit\" value=\"Submit\"/>\n" +
-                "</form>" +
-                "\n" +
+                "</form>\n" +
                 "</body>\n" +
                 "</html>";
 
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:8080/find") ;
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:9090/slackbot") ;
         assertEquals(200, ActualHttpResponse.getStatusCode());
         assertEquals(expectedResponse, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForGETWithWrongPath1() {
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:8080/");
+    public void testSlackBotForGETWithWrongPath1() {
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:9090/");
         assertEquals(404, ActualHttpResponse.getStatusCode());
         assertEquals(PathNotFoundResponse404, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForGETWithWrongPath2() {
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:8080/find?asin=abc");
+    public void testSlackBotForGETWithWrongPath2() {
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:9090/slackbot?asin=abc");
         assertEquals(404, ActualHttpResponse.getStatusCode());
         assertEquals( PathNotFoundResponse404, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForGETWithWrongPath3() {
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:8080/search");
+    public void testSlackBotForGETWithWrongPath3() {
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doGet("http://localhost:9090/SLACKBOT");
         assertEquals(404, ActualHttpResponse.getStatusCode());
         assertEquals( PathNotFoundResponse404, ActualHttpResponse.getResponseMessage());
     }
 
-
     @Test
-    public void testFindAsinForPOSTWithWrongPath() {
+    public void testSlackBotForPOSTWithWrongPath() {
         HashMap<String, String> headers = new HashMap<>();
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:8080/finder", headers, "asin=120401325X");
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:9090/slackboat", headers, "message=INCORRECT PATH");
         System.out.println(ActualHttpResponse);
         System.out.println(ActualHttpResponse.getStatusCode());
         assertEquals(404, ActualHttpResponse.getStatusCode());
@@ -129,70 +109,72 @@ public class SearchApplicationFindTest {
     }
 
     @Test
-    public void testFindAsinForMethodNotAllowed() {
+    public void testSlackBotForMethodNotAllowed() {
         HashMap<String, String> headers = new HashMap<>();
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doDelete("http://localhost:8080/find");
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doDelete("http://localhost:9090/slackbot");
         assertEquals(405, ActualHttpResponse.getStatusCode());
         assertEquals( MethodNotAllowedResponse405, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForPOSTWithCorrectPathIncorrectRequest1() {
+    public void testSlackBotForPOSTWithCorrectPathIncorrectRequest1() {
         HashMap<String, String> headers = new HashMap<>();
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:8080/find", headers, "asinNumber=120401325X");
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:9090/slackbot", headers, "message = INCORRECT=REQUEST1");
         assertEquals(400, ActualHttpResponse.getStatusCode());
         assertEquals(BadRequestResponse400, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForPOSTWithCorrectPathIncorrectRequest2() {
+    public void testSlackBotForPOSTWithCorrectPathIncorrectRequest2() {
         HashMap<String, String> headers = new HashMap<>();
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:8080/find", headers, "asin = 120401325X");
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:9090/slackbot", headers, " message=INCORRECT=REQUEST2");
         assertEquals(400, ActualHttpResponse.getStatusCode());
         assertEquals(BadRequestResponse400, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForPOSTWithCorrectPathIncorrectRequest3() {
+    public void testSlackBotForPOSTWithCorrectPathIncorrectRequest3() {
         HashMap<String, String> headers = new HashMap<>();
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:8080/find", headers, " asin=120401325X");
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:9090/slackbot", headers, " message~INCORRECT=REQUEST3");
         assertEquals(400, ActualHttpResponse.getStatusCode());
         assertEquals(BadRequestResponse400, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForPOSTWithCorrectPathIncorrectRequest4() {
+    public void testSlackBotForPOSTWithCorrectPathIncorrectRequest4() {
         HashMap<String, String> headers = new HashMap<>();
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:8080/find", headers, "asin-120401325X");
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:9090/slackbot", headers, "mess=INCORRECT=REQUEST4");
         assertEquals(400, ActualHttpResponse.getStatusCode());
         assertEquals(BadRequestResponse400, ActualHttpResponse.getResponseMessage());
     }
 
     @Test
-    public void testFindAsinForPOSTWithCorrectPathCorrectRequest() {
-        String expectedRepose = generateHTMLResponseForPOST("Find ASIN", fileOps.findAsin("120401325X") );
+    public void testSlackBotForPOSTWithCorrectPathCorrectRequest() {
+        String message = "This is a CORRECT Request Message From ChatApplicationTest, TestResult=PASSED";
+        String expectedRepose = generateHTMLResponseForPOST(message);
         HashMap<String, String> headers = new HashMap<>();
-        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:8080/find", headers, "asin=120401325X");
+        message = "message="+ message;
+        HTTPResponse ActualHttpResponse = HTTPTestClient.doPost("http://localhost:9090/slackbot", headers, message);
         assertEquals(200, ActualHttpResponse.getStatusCode());
         assertEquals(expectedRepose, ActualHttpResponse.getResponseMessage());
     }
 
-    private String generateHTMLResponseForPOST(String title, String body) {
+    private String generateHTMLResponseForPOST(String message) {
         String HTMLResponseMessage = "<!DOCTYPE html>\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                 "<head>\n" +
-                "<title>" + title + " </title>\n" +
-                "<h2><u>" + title + " </u></h2>\n" +
+                "  <title>Slack Bot</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<p>" ;
-
-        String[] lines = body.split("\n");
-        for(String line: lines){
-            HTMLResponseMessage += line + "<br/>" ;
-        }
-
-        HTMLResponseMessage += "</p>\n" +
+                "\n" +
+                "<h1><u>Slack Bot</u></h1>" +
+                "<p> MESSAGE <b>'" + message + "'</b> SENT SUCCESSFULLY ON SLACK CHANNEL!</p>" +
+                "<form action=\"/slackbot\" method=\"post\">\n" +
+                "  <label for=\"msg\"><b>Send another Message:</b></label><br/><br/>\n" +
+                "  <input type=\"text\" id=\"message\" name=\"message\"/><br/><br/>\n" +
+                "  <input type=\"submit\" value=\"Submit\"/>\n" +
+                "</form>" +
+                "\n" +
                 "</body>\n" +
                 "</html>";
 
